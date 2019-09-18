a335eecab6b738188ca6549144a75de05c7bec07
Victoria Collier <victoriacollier@Victorias-MacBook-Pro.local>
2019-09-17 20:39:54 -0400

    adding development files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 (() => {
 	// stub
 	console.log('fired');
@@ -5,12 +6,15 @@
 	const 	pictures 		= document.querySelectorAll('.pictures'),
 			pic1 	= document.querySelector('.pic1'),
 			pic2	= document.querySelector('.pic2'),
+			pic3   = document.querySelector('.pic3'),
 			lightbox 	= document.querySelector('.lightbox'),
 			box1 	= document.querySelector('.box1'),
 			box2 	= document.querySelector('.box2'),
+			box3    = document.querySelector('.box3'),
 			image		= document.querySelector('img'),
 			closeButton1 = document.querySelector('.closeButton1');
 			closeButton2 = document.querySelector('.closeButton2');
+			closeButton3 = document.querySelector('.closeButton3');
 
 	function openLightbox1() {
 		box1.classList.add('box1-on');
@@ -18,6 +22,10 @@
 
 	function openLightbox2() {
 		box2.classList.add('box2-on');
+	}
+
+	function openLightbox3() {
+		box3.classList.add('box3-on');
 	}
 	
 	function closeLightbox1() {
@@ -28,11 +36,17 @@
 		box2.classList.remove('box2-on');
 	}
 
+	function closeLightbox3() {
+		box3.classList.remove('box3-on');
+	}
+
 
 	pic1.addEventListener('click', openLightbox1);
 	pic2.addEventListener('click', openLightbox2);
+	pic3.addEventListener('click', openLightbox3);
 	closeButton1.addEventListener('click', closeLightbox1);
 	closeButton2.addEventListener('click', closeLightbox2);
+	closeButton3.addEventListener('click', closeLightbox3);
 
 
 })();
---
+++ js/modules/dataEntry.js
@@ -0,0 +1,25 @@
+const Team = {
+    
+    Victoria: {
+        name: "Victoria",
+        bio: "I don't know where i am... can you please take me home",
+        role: "Designer",
+        pic: "victoria.jpg"},
+
+    Renata: {
+        name: "Renata",
+        bio: "I love hunny",
+        role: "Designer",
+        pic: "renata.jpeg"},
+
+    Keiran: {
+        name: "Jarrod",
+        bio: "I have more than one look",
+        role: "Developer",
+        pic: "keiran.jpg"},
+
+}
+
+
+// export {team, people};
+export default Team;
