a2af67d37f02bbdb2d7936af775c006ca20da468
kmankeiran <43183043+kmankeiran@users.noreply.github.com>
2019-09-19 20:17:07 -0400

    Add files via upload
    Adding buttons to control the video and tweaking the CSS a little bit.

--- js/main.js
+++ js/main.js
@@ -12,9 +12,13 @@
 			box2 	= document.querySelector('.box2'),
 			box3    = document.querySelector('.box3'),
 			image		= document.querySelector('img'),
-			closeButton1 = document.querySelector('.closeButton1');
-			closeButton2 = document.querySelector('.closeButton2');
-			closeButton3 = document.querySelector('.closeButton3');
+			closeButton1 = document.querySelector('.closeButton1'),
+			closeButton2 = document.querySelector('.closeButton2'),
+			closeButton3 = document.querySelector('.closeButton3'),
+			teamVideo = document.querySelector('.teamVid'),
+			pauseButton = document.querySelector(".pauseButton"),
+    		playButton = document.querySelector(".playButton"),
+    		rewind = document.querySelector(".replayButton");
 
 	function openLightbox1() {
 		box1.classList.add('box1-on');
@@ -40,12 +44,37 @@
 		box3.classList.remove('box3-on');
 	}
 
+	function pauseVideo() {
+    	teamVideo.pause();
+	}
+
+	function playVideo() {
+		teamVideo.play();
+	}
+
+	function rewindVideo() {
+		teamVideo.currentTime = 0;
+		playVideo();
+	}
+
+
+	
+	pauseButton.addEventListener("click", pauseVideo);
+
+	playButton.addEventListener("click", playVideo);
+
+	rewind.addEventListener("click", rewindVideo);
 
 	pic1.addEventListener('click', openLightbox1);
+
 	pic2.addEventListener('click', openLightbox2);
+
 	pic3.addEventListener('click', openLightbox3);
+
 	closeButton1.addEventListener('click', closeLightbox1);
+
 	closeButton2.addEventListener('click', closeLightbox2);
+
 	closeButton3.addEventListener('click', closeLightbox3);
 
 
